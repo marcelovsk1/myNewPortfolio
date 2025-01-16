9e658072aa677fd56158a5e0be55af4581b33d51
Marcelo Amaral Alves <marceloaa@live.com>
2025-01-15 21:45:19 -0500

    projects

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/Navbar/Navbar"
 import Intro from "./Components/Intro/Intro"
 import About from "./Components/About/About"
 import Experience from "./Components/Experience/Experience"
+import Projects from "./Components/Projects/Projects"
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Intro/>
       <About/>
       <Experience/>
+      <Projects/>
+
     </div>
   );
 }
